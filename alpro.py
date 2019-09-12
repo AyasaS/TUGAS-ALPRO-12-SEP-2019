559b34c625fc929e9a8489c10cda74ccc5b7eb1e
AyasaS <54665946+AyasaS@users.noreply.github.com>
2019-09-12 08:46:14 +0700

    Syahrastani Ayasa TI-42-11 1201182467
    TUGAS ALPRO 12 SEPTEMBER 2019

---
+++ alpro.py
@@ -0,0 +1,15 @@
+GPA = [3, 2.7, 2.5, 4]
+
+def Bonus():
+    TotalBonus = x * B 
+    return TotalBonus
+
+B = 500000
+
+for x in GPA :
+    print(x)
+    if x > 3:
+        print (x, "anda mendapatkan bonus Rp. ", Bonus())
+    elif x <= 3:
+        print ("anda tidak mendapatkan bonus")
+
